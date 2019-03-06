540de25b77465e949cfd4510c872c8846a433908
Matthew Eichler <matthew.eichler@aventinesolutions.nl>
2019-03-06 08:24:52 +0100

    :bear: :construction: fix the descriptions in the Project configuration
    US1 As Tickets4Sale, I want to know the ticket status of a performance for a show, so that I can ...

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject tickets4sale "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Coding Challenge for Vakantie Discounter and Otravo (Spilberg)"
+  :url "https://gitlab.com/aventinesolutions/tickets4sale"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]]
