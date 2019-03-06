1010119cf73fda88e2076f8c86546715b8f9d67b
Matthew Eichler <matthew.eichler@aventinesolutions.nl>
2019-03-06 17:52:56 +0100

    :bear: :construction: add "component", "aleph", "hiccup" and "bidi"
    ... dependencies
    
    US1 As Tickets4Sale, I want to know the ticket status of a performance for a show, so that I can ...

--- project.clj
+++ project.clj
@@ -1,9 +1,14 @@
 (defproject tickets4sale "0.1.0-SNAPSHOT"
   :description "Coding Challenge for Vakantie Discounter and Otravo (Spilberg)"
   :url "https://gitlab.com/aventinesolutions/tickets4sale"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :license
+  {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+   :url  "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [com.stuartsierra/component "0.4.0"]
+                 [aleph "0.4.6"]
+                 [hiccup "1.0.5"]
+                 [bidi "2.1.5"]]
   :main ^:skip-aot tickets4sale.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
