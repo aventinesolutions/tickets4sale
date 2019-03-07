7d19b050149a8fe665f364f4c3960cec51e5554d
Matthew Eichler <matthew.eichler@aventinesolutions.nl>
2019-03-07 07:56:19 +0100

    :bear: :construction: add Java IO and CSV dependencies
    US1 As Tickets4Sale, I want to know the ticket status of a performance for a show, so that I can ...

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
   {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
    :url  "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.clojure/data.csv "0.1.4"]
                  [com.stuartsierra/component "0.4.0"]
                  [aleph "0.4.6"]
                  [hiccup "1.0.5"]
