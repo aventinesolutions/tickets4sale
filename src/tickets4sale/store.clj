9a9c1fb12e06b9e76340dbb68b0cc44c98ede650
Matthew Eichler <matthew.eichler@aventinesolutions.nl>
2019-03-07 07:57:01 +0100

    :bear: :construction: function for initializing store from CVS file
    US1 As Tickets4Sale, I want to know the ticket status of a performance for a show, so that I can ...

--- src/tickets4sale/store.clj
+++ src/tickets4sale/store.clj
@@ -1,5 +1,12 @@
 (ns tickets4sale.store
-  (:require [com.stuartsierra.component :as component]))
+  (:require [com.stuartsierra.component :as component]
+            [clojure.data.csv :as csv]
+            [clojure.java.io :as io]))
+
+(defn initialize-from-csv "initializes store from given CVS file" [path]
+  (with-open [reader (io/reader path)]
+    (doall
+     (csv/read-csv reader))))
 
 (defrecord InMemoryStore [data]
 
