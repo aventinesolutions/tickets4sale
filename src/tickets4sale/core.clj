7976283baba8b719807dcb5ed9f37862b8580513
Matthew Eichler <matthew.eichler@aventinesolutions.nl>
2019-03-08 08:31:11 +0100

    :bear: :construction: essentials for the main runner
    US-1: As Tickets4Sale, I want to know the ticket status of a
    performance for a show, so that I can service my customers well.

--- src/tickets4sale/core.clj
+++ src/tickets4sale/core.clj
@@ -1,7 +1,10 @@
 (ns tickets4sale.core
-  (:gen-class))
+  (:gen-class)
+  (:require [tickets4sale.system :as system]
+            [clojure.string :as string]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn -main "cli path-to-csv-file: start and use a CLI system" [& args]
+  (if
+    (= "cli" (string/lower-case (first args)))
+    (system/init-cli-system (nth args 1))
+    (println "don't know what to do")))
