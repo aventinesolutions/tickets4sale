602a7ed1ecb4306c266b74d6d578184fb6057cff
Matthew Eichler <matthew.eichler@aventinesolutions.nl>
2019-03-11 10:52:02 +0100

    :bear: :construction: some basic structure for domain
    US-1: As Tickets4Sale, I want to know the ticket status of a performance for a show, so that I can service my customers well.

--- project.clj
+++ project.clj
@@ -7,6 +7,7 @@
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/data.csv "0.1.4"]
                  [com.stuartsierra/component "0.4.0"]
+                 [clojure.java-time "0.3.2"]
                  [aleph "0.4.6"]
                  [hiccup "1.0.5"]
                  [bidi "2.1.5"]]
---
+++ src/tickets4sale/domain.clj
@@ -0,0 +1,40 @@
+; the business domain
+(ns tickets4sale.domain
+  (:require [java-time :as time]
+            [clojure.instant :as instant]))
+
+(def show-run-in-days 100)
+
+(def move-to-smaller-venue-after-days 60)
+
+(def larger-venue-capacity-seats 200)
+
+(def smaller-venue-capacity-seats 100)
+
+(def tickets-sold-per-day-bigger-venue 25)
+
+(def tickets-sold-per-day-smaller-venue 5)
+
+(defn ticket-sales-start
+  "the day that ticket sales will/did start"
+  [show-start])
+
+(defn venue-type
+  "the venue type based on the number of days the show has been running"
+  [show-start])
+
+(defn tickets-left
+  "the number of tickets left based on days running"
+  [show-start])
+
+(defn show-status
+  "the status of the show based on start and days running"
+  [show-start])
+
+(defn tickets-available
+  "the number of tickets available based on tickets that should have been sold"
+  [show-start])
+
+(defn tickets-sold
+  "the number of tickets presumable sold"
+  [show-start])
