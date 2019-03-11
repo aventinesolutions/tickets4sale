7858695c2d89f6d4e66d733a037981515c915884
Matthew Eichler <matthew.eichler@aventinesolutions.nl>
2019-03-11 19:56:47 +0100

    :bear: :construction: status "sale not started" with test :green_heart:
    US-1: As Tickets4Sale, I want to know the ticket status of a performance for a show, so that I can service my customers well.

--- src/tickets4sale/domain.clj
+++ src/tickets4sale/domain.clj
@@ -1,7 +1,6 @@
 ; the business domain
 (ns tickets4sale.domain
-  (:require [java-time :as time]
-            [clojure.instant :as instant]))
+  (:require [java-time :as time]))
 
 (def show-run-in-days 100)
 
@@ -11,39 +10,43 @@
 
 (def smaller-venue-capacity-seats 100)
 
-(def sales-start-days-before-opening 25)
+(def sales-start-days-before-opening (time/days 25))
 
 (def tickets-sold-per-day-bigger-venue 10)
 
 (def tickets-sold-per-day-smaller-venue 5)
 
-(defn ticket-sales-started?
-  "have the ticket sales started?"
-  [query-date show-start]
-
-  )
-
 (defn ticket-sales-start
   "the day that ticket sales will/did start"
-  [query-date show-start]
-  )
+  [show-opening]
+  (time/minus show-opening sales-start-days-before-opening))
+
+(defn ticket-sales-started?
+  "have the ticket sales started?"
+  [query-date show-opening]
+  (let [sales-start (ticket-sales-start show-opening)]
+    (or (= query-date sales-start)
+        (time/after? query-date sales-start))))
 
 (defn venue-type
   "the venue type based on the number of days the show has been running"
-  [show-start])
+  [show-opening])
 
 (defn tickets-left
   "the number of tickets left based on days running"
-  [show-start])
+  [show-opening])
 
 (defn show-status
   "the status of the show based on start and days running"
-  [show-start])
+  [query-date show-opening]
+  (if (not (ticket-sales-started? query-date show-opening))
+    "sale not started"
+    "unknown"))
 
 (defn tickets-available
   "the number of tickets available based on tickets that should have been sold"
-  [show-start])
+  [show-opening])
 
 (defn tickets-sold
   "the number of tickets presumable sold"
-  [show-start])
+  [show-opening])
--- test/tickets4sale/domain_test.clj
+++ test/tickets4sale/domain_test.clj
@@ -1,10 +1,11 @@
 (ns tickets4sale.domain-test
   (:require [clojure.test :refer :all]
-            [tickets4sale.domain :refer :all]))
+            [tickets4sale.domain :refer :all]
+            [java-time :as time]))
 
-(deftest domain
+(deftest domain-tests
 
-  (deftest domain-constants
+  (deftest domain-constants-tests
     (testing "the number of days a show always runs"
              (is (= 100 show-run-in-days)))
     (testing "venue is always moved to the smaller after days"
@@ -14,16 +15,41 @@
     (testing "the smaller venue always has seats capacity"
              (is (= 100 smaller-venue-capacity-seats)))
     (testing "sales start the number of days before the show starts is always"
-             (is (= 25 sales-start-days-before-opening)))
+             (is (= (time/days 25) sales-start-days-before-opening)))
     (testing "the number of tickets sold per day for larger venues is always"
              (is (= 10 tickets-sold-per-day-bigger-venue)))
     (testing "the number of tickets sold per day for smaller venus is always"
              (is (= 5 tickets-sold-per-day-smaller-venue))))
 
-  (deftest ticket-sales-start-test
-    (testing "provides a date if sales starts" (is (true? false)))
-    (testing "nil if sales have not yet started" (is (true? false))))
+  (let [show-opening (time/local-date "2019-01-15")]
+    (deftest ticket-sales-start-test
+      (testing "provides a date sales will/did start"
+               (is
+                (=
+                 (time/local-date "2018-12-21")
+                 (ticket-sales-start show-opening)))))
 
-  (deftest ticket-sales-started?-test
-    (testing "true if sales have started" (is (true? false)))
-    (testing "fals if sales have not yet started" (is (true? false)))))
+    (let [query-date (time/local-date "2018-12-31")]
+      (deftest ticket-sales-started?-test
+        (testing "true if sales have started based on query date"
+                 (is
+                  (true?
+                   (ticket-sales-started? query-date show-opening))))
+        (testing "true if sales started exeactly on query date"
+                 (is
+                  (true?
+                   (ticket-sales-started?
+                    (time/local-date "2018-12-21") show-opening))))
+        (testing "false if sales have not yet started base on query date"
+                 (is
+                  (false?
+                   (ticket-sales-started?
+                    query-date (time/local-date "2818-12-12"))))))))
+
+  (let [show-opening (time/local-date "2019-02-02")]
+    (deftest show-status-tests
+      (testing "\"sale not started \" when query date is before sale start date"
+               (is
+                (=
+                 "sale not started"
+                 (show-status (time/local-date "2019-01-03") show-opening)))))))
