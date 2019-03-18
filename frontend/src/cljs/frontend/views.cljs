adcc2cc0362b36570855cd3d56827e14292c2bcc
Matthew Eichler <matthew.eichler@aventinesolutions.nl>
2019-03-18 09:46:29 +0100

    :bear: :construction: basic show date input form
    US-2: As Tickets4Sale service employee, I want to have a web site,
    so that I can check the ticket status of a performance for a show.

--- frontend/src/cljs/frontend/views.cljs
+++ frontend/src/cljs/frontend/views.cljs
@@ -3,7 +3,17 @@
     [re-frame.core :as re-frame]
     [frontend.subs :as subs]))
 
+(defn date-input
+  "component for inputting the show date"
+  []
+  [:form
+   [:label "Please enter a show date"
+    [:input {:type :text :value "YYYY-MM-DD" :width 10}]
+    [:input {:type :submit :value "Query ticket status"}]]])
+
 (defn main-panel []
   (let [name (re-frame/subscribe [::subs/name])]
     [:div
-     [:h1 "Welcome to " @name]]))
+     [:h1 "Welcome to " @name]
+     [:div [date-input]]]))
+
